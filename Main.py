f66fe09808c7f21bdacfd9e12b5fe92063079cf0
sami-arifuz <samiarifuzzaman@gmail.com>
2025-04-15 10:57:46 +0100

    Update Main.py
    Added two new def for search student by name and sort student by their marks

--- Main.py
+++ Main.py
@@ -12,4 +12,17 @@ def add_student(name, mark):
   if not isinstance(mark, (int, float)) or mark < 0 or mark > 100:
     raise ValueError("Mark should be between 0 and 100.")
   students.append({'name': name.strip(), 'mark': mark})
+
+def search_student(name):
+  return [s for s in students if name.lower() in s['name'].lower()]
+
+def sort_students_by_marks():
+  students.sort(key=lambda x: x['mark'], reverse=True)
+
+
+  
+        
+    
+  
+  
     
